129ab3357d7412908f35619da32c37f35dd5f429
Hiếu Nguyễn <valkyrie241296@gmail.com>
2018-07-25 22:53:30 +0700

    no message

--- NavigationController/ViewController.swift
+++ NavigationController/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func presentVC(_ sender: UIButton) {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let screenBlue = sb.instantiateViewController(withIdentifier: "BLUE") as! BlueViewController
+        self.navigationController?.present(screenBlue, animated: true, completion: nil)
+    }
+    @IBAction func pushVC(_ sender: Any) {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let screenBlue = sb.instantiateViewController(withIdentifier: "BLUE") as! BlueViewController
+        self.navigationController?.pushViewController(screenBlue, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
